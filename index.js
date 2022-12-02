cc0845359cd7884a2b54a41fbe188edf64c16ac4
Anyanime Benson <bensonanyanime@gmail.com>
2022-12-02 18:52:57 +0100

    feat: Add function to clear text from webapp and Add comment to code

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-
+//create document element
 let p = document.createElement('p');
 let words = document.querySelector('.output');
 words.appendChild(p);
 
+//Display transcript on webapp
 recognition.addEventListener("result", e => {
     const statement = Array.from(e.results)
         .map(result => result[0])
@@ -23,5 +24,18 @@ recognition.addEventListener("result", e => {
         } 
 })
 
+let btn = document.querySelector('button');
+
+//Add function to clear text
+let clearText = () => {
+    while(words.hasChildNodes()) {
+       words.removeChild(words.lastChild)
+    }
+   }
+
+//initialise recognition
 recognition.start();
-recognition.addEventListener('click', recognition.start)
+
+//Add Event listeners
+recognition.addEventListener('end', recognition.start)
+btn.addEventListener('click', clearText)
